6177e1c4a4022dc626f5e0fecdee1498c26fcd12
nicka <nicka@DESKTOP-6CBE75G.home>
2021-09-20 10:53:09 -0400

    Lab 2 attempt 4

--- src/main/java/bcs/csc229_lab_2/Lab_2_Main.java
+++ src/main/java/bcs/csc229_lab_2/Lab_2_Main.java
@@ -23,6 +23,10 @@ public class Lab_2_Main {
      
         Scanner input = new Scanner(System.in);//scanner for input
         
+        
+        Lab2_Pairwise t1 = new Lab2_Pairwise();
+        
+        
         System.out.println("Please enter the size of the array: ");
         int size = input.nextInt();//this will get the determine the size of the array based on user input
         
@@ -34,24 +38,8 @@ public class Lab_2_Main {
             pairwiseArr[i] = input.nextInt();
             
         }
-        
-        int maxNum1 = -1;//max num 1 for determining pairwise product
-        int maxNum2 = -1;// max num 2 for determining pairwise product
-        
-        for(int i = 0; i < pairwiseArr.length; i++){//for loop for finding the first big element in the array
-            if((maxNum1 == -1) || (pairwiseArr[i] > pairwiseArr[maxNum1])){//if the maxNum is -1 or pairwise at index i is biggert than pairwise at maxNum then...
-                maxNum1 = i;//max num equals i
-            }
-        }
-        for(int i = 0; i < pairwiseArr.length; i++){//for loop for finding the second big element in the array
-            if((i != maxNum1) &&  ((maxNum2 == -1) || (pairwiseArr[i] > pairwiseArr[maxNum2]))){//if i does not equal maxNum 1 AND max num2 is -1 or pairwiseArr at index i is biggeer than pairwiseArr at index maxnum2 then...
-                maxNum2 = i;//maxNum2 = i
-            }
-        }
-        
-        int finalProduct = pairwiseArr[maxNum1] * pairwiseArr[maxNum2];//this will determine the maximum product for the array
-        
-        System.out.println("The biggest product is: " + finalProduct);//prints the product
+      
+        System.out.println("The biggest product is: " + t1.calcHighestPairwise(size, pairwiseArr));//this will determine the two highest elemetns in the array and find the biggest products
         
         Instant f = Instant.now();//timer ended
         long timeElapsed = Duration.between(s, f).toMillis();//calculation finsihed 
